6f122b0fd223f5ca9a32253be6f6f751e4d67268
Dionei Bianchati <dion.biancha@gmail.com>
2022-01-07 10:27:48 -0300

    feature: calendar

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import "react-calendar-heatmap/dist/styles.css";
 
-import GlobalStyles from './styles/GlobalStyles';
-import Header from './components/Header';
-import Profile from './pages/Profile';
-import Repo from './pages/Repo';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import GlobalStyles from "./styles/GlobalStyles";
+import Header from "./components/Header";
+import Profile from "./pages/Profile";
+import Repo from "./pages/Repo";
 
 function App() {
   return (
     <BrowserRouter>
-    <Header />
+      <Header />
       <Routes>
         <Route path="/" element={<Profile />} />
         <Route path="/:username" element={<Profile />} />
--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,7 +1,60 @@
-import React from 'react';
+import React from "react";
+
+import {
+  Container,
+  Main,
+  LeftSide,
+  RightSide,
+  Repos,
+  CalendarHeading,
+} from "./styles";
+
+import ProfileData from "../../components/ProfileData";
+import RepoCard from "../../components/RepoCard";
+import RandomCalendar from "../../components/RandomCalendar";
 
 const Profile: React.FC = () => {
-  return <div />
+  return (
+    <Container>
+      <Main>
+        <LeftSide>
+          <ProfileData
+            username={"dionbiancha"}
+            name={"Dionei Bianchati"}
+            avatarUrl={"https://avatars.githubusercontent.com/u/47863625?v=4"}
+            followers={965}
+            following={34}
+            company={"Freelancer"}
+            location={"Missal, Brazil"}
+            email={"contato@dionei.com"}
+            blog={"dionei.com"}
+          />
+        </LeftSide>
+        <RightSide>
+          <Repos>
+            <h2>Random Repos</h2>
+            <div>
+              {[1, 2, 3, 4, 5, 6].map((n) => (
+                <RepoCard
+                  key={n}
+                  username={"dionbiancha"}
+                  reponame={"youtube-content"}
+                  description={"Contains all of my YouTube lessons code"}
+                  language={n % 3 === 0 ? "Javascript" : "TypeScript"}
+                  stars={8}
+                  forks={4}
+                />
+              ))}
+            </div>
+          </Repos>
+          <CalendarHeading>
+            Random calendar (do not represent actual data)
+          </CalendarHeading>
+          <RandomCalendar />
+        </RightSide>
+      </Main>
+    </Container>
+  );
 };
 
 export default Profile;
